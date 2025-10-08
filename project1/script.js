637e4b73ec05cb40c4ce8fa975db883297f49d0f
Diego D'Ortenzio <diegodorte2007@gmail.com>
2025-10-08 21:29:47 +0200

    Aggiunta nuovi metodi
    Mancante form aggiunta singolarmente, salvataggio elemento modificato o altro

--- project1/script.js
+++ project1/script.js
@@ -1,3 +1,9 @@
+let arrayIdPrivati=[]; 
+const salvati = sessionStorage.getItem("arrayIdPrivati"); //uso le sessioni perchè mi sono più comode
+if (salvati) {
+    arrayIdPrivati = JSON.parse(salvati);
+}
+
 function eseguiRichiesta() { //Metodo che riprende il codice di postman
     const requestOptions = {
         method: "GET",
@@ -15,12 +21,71 @@ function eseguiRichiesta() { //Metodo che riprende il codice di postman
     });
 }
 
+function eseguiAggiunta(stringaJsonAggiunta){
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = stringaJsonAggiunta; //stringa json che prevedo sia già convertita in json
+
+    const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+    };
+
+    fetch("https://api.restful-api.dev/objects", requestOptions)
+    .then((response) => {
+        if (!response.ok){
+            const contenuto = document.getElementById("cont-contenuto");
+            contenuto.style.visibility = "visible";
+            contenuto.innerText = `Errore ${response.status}: Impossibile aggiungere l'oggetto.`;
+            setTimeout(() => { 
+                contenuto.innerText = ""; 
+                contenuto.style.visibility = "hidden"; 
+                window.location.reload();
+            }, 120000); //2 minuti
+            return;
+        }
+    })
+    .then((result) => {
+        if(result != null){
+            const ogg = JSON.parse(result);
+            arrayIdPrivati.push(ogg.id); //salvo id privato
+            sessionStorage.setItem("arrayIdPrivati", JSON.stringify(arrayIdPrivati)); 
+            eseguiRichiesta();
+        }
+    })
+    .catch((error) => console.error(error));
+}
+
 function mostraContenuto(result) { //result è un array di oggetti convertito prima
     const contenuto = document.getElementById("cont-contenuto");
     contenuto.style.visibility = "visible";
     for( let ogg of result){
         contenuto.appendChild(new Oggetto(ogg.id, ogg.name, ogg.data).creaElementoGrafica());
-    }   
+    }  
+    
+    if(arrayIdPrivati.length > 0){ 
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        let stringaRichiesta="https://api.restful-api.dev/objects/";
+        for(let idp of arrayIdPrivati){
+            stringaRichiesta+=`id=${idp}&`;
+        }
+        fetch(stringaRichiesta, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            const oggPriv = JSON.parse(result);
+            for(let ogg of oggPriv){
+                contenuto.appendChild(new Oggetto(ogg.id, ogg.name, ogg.data).creaElementoGrafica());
+            }
+        })
+        .catch((error) => console.error(error));
+    }
 }
 
 function aggiungiTornaIndietro(){
@@ -55,6 +120,36 @@ class Oggetto {
         const elemento = document.createElement("div");
         elemento.className = "oggetto_restituito";
         elemento.innerText = this.toString();
+        return elemento;
+    }
+
+    creaElementoModificaGrafica(){ 
+        const elemento = document.createElement("div");
+        elemento.className = "oggetto_restituito";
+        elemento.innerText = this.toString();
+        const bottone = document.createElement("button");
+        bottone.innerText = "Modifica";
+        bottone.onclick = function() { 
+            const contenuto = document.getElementById("cont-contenuto");
+            contenuto.style.visibility = "hidden";
+
+            const formModifica = document.getElementById("formmodifica");
+            formModifica.style.visibility = "visible";
+            document.getElementById("id-form").value = this.id;
+            document.getElementById("nome-form").value = this.nome;
+            for (const [key, value] of Object.entries(this.data)){
+                const hString = document.createElement("h2");
+                hString.innerText = key;
+                const inputString = document.createElement("input");
+                inputString.type = "text";
+                inputString.id = `form-${key}`;
+                inputString.placeholder = `Inserisci ${key}`;
+                inputString.value = value;
+                formModifica.appendChild(hString);
+                formModifica.appendChild(inputString);
+            }
+         };
+        elemento.appendChild(bottone);
         return elemento;
     }
 }
